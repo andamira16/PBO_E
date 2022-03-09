543406495f0527b204c61929846b308513935943
andamira16 <100887721+andamira16@users.noreply.github.com>
2022-03-09 18:50:32 +0700

    Add files via upload

--- latihan1/src/main/java/id/ac/uin/naruto/latihan1/IndexController.java
+++ latihan1/src/main/java/id/ac/uin/naruto/latihan1/IndexController.java
@@ -3,13 +3,16 @@ package id.ac.uin.naruto.latihan1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping ("api")
 public class IndexController {
     @GetMapping("hello")
-    public String hello(){
-        return"Selamat Belajar Spring Boot!";
+    public String hello(@RequestParam String name){
+        return"Selamat Belajar Spring Boot!"+ name +" " + LocalDateTime.now();
     }
 }

fbcb8d7b748af8f3b5b9555ac281fac4a3cd9505
andamira16 <100887721+andamira16@users.noreply.github.com>
2022-03-09 15:08:26 +0700

    Add files via upload

--- latihan1/src/main/java/id/ac/uin/naruto/latihan1/Latihan1Application.java
+++ latihan1/src/main/java/id/ac/uin/naruto/latihan1/Latihan1Application.java
@@ -2,7 +2,6 @@ package id.ac.uin.naruto.latihan1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Latihan1Application {
 
